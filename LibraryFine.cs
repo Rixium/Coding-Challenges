a520dcd5ab41695116e17bc11c07ba1ee979b0bf
Rixium <Gamerpotty@gmail.com>
2018-02-18 17:36:18 +0000

    Removed unnecessary use of Math.Abs from LibraryFine.

--- LibraryFine.cs
+++ LibraryFine.cs
@@ -17,7 +17,7 @@ class Solution {
     static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
         if(y1 > y2) return 10000;
         if(y1 < y2 || m1 < m2 || (d1 < d2 && m1 == m2)) return 0;
-        if(m1 > m2) return Math.Abs(m1 - m2) * 500;
+        if(m1 > m2) return (m1 - m2) * 500;
         return Math.Abs(d1 - d2) * 15;
     }
 
